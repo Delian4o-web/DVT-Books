bf781ef463e7c52c5bff99939fc2bc2f2d277af7
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-24 09:50:32 +0200

    Fix linting

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { AuthService } from "../services/auth.service";
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
-  selector: "app-home-page",
-  templateUrl: "./home-page.component.html",
-  styleUrls: ["./home-page.component.scss"],
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
   constructor(public auth: AuthService) {}
