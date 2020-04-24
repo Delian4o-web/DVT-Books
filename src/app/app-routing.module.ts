effa875257580114a21f1a5d3d788f731f3213cb
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-24 13:20:02 +0200

    Change routes for home page and login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-
+import { UsersPageComponent } from './users-page/users-page.component';
 const routes: Routes = [
   { path: 'home', component: HomePageComponent },
-  { path: '**', redirectTo: 'home' },
+  { path: 'profile', component: UsersPageComponent },
+  { path: '**', redirectTo: 'profile' },
 ];
 
 @NgModule({
