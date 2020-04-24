5dfd2c891d30160dddb3ac797a1b798da644fa28
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-24 10:02:49 +0200

    Fix Linting errors

--- src/app/home-page/home-page.component.spec.ts
+++ src/app/home-page/home-page.component.spec.ts
@@ -1,8 +1,8 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomePageComponent } from "./home-page.component";
+import { HomePageComponent } from './home-page.component';
 
-describe("HomePageComponent", () => {
+describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
 
