cb24560726ac91b2071fe1fc7c9239e2978f1e77
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-28 16:30:37 +0200

    Add user profile model

---
+++ src/app/models/userprofile.ts
@@ -0,0 +1,10 @@
+export class UserProfile {
+  'http://localhost:4200/claims/role': string[];
+  nickname: string;
+  name: string;
+  picture: string;
+  updated_at: string;
+  email: string;
+  email_verified: boolean;
+  sub: string;
+}
