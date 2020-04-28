5443bb11de6a2031bcc6715818f5d89a1857b1de
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-28 16:32:30 +0200

    Add enviroment variable for namespace

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ export const environment = {
   domain: 'delyan4o.auth0.com',
   client_id: 'pG4P6sbeE1z3cAjLwWfGJbvGGGNgIbvT',
   apiUri: 'http://localhost:4201',
+  nameSpace: 'http://localhost:4200/claims/role',
 };
