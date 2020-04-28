1bb5d66ed3943b54d63c3f6c2276eb3c0d045d7b
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-28 12:13:18 +0200

    Remove unused imports

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { AuthorService } from '../services/author.service';
+import { Author } from '../models/author';
 
 @Component({
   selector: 'app-home-page',
