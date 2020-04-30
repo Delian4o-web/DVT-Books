2bd96bc964e6c563ec269e6ed32f209dcd3b9fde
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-30 09:28:06 +0200

    Linting Changes

--- src/app/models/author.ts
+++ src/app/models/author.ts
@@ -3,8 +3,11 @@ import { Book } from './book';
 export class Author {
   href: string;
   id: string;
+  // tslint:disable-next-line: variable-name
   first_name: string;
+  // tslint:disable-next-line: variable-name
   middle_names: string;
+  // tslint:disable-next-line: variable-name
   last_name: string;
   name: string;
   about: string;
