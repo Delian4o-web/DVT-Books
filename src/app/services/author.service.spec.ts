106e1894ca00083093f0327f413b0dda6ddcc95b
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-30 13:53:36 +0200

    Add unit test to author service

--- src/app/services/author.service.spec.ts
+++ src/app/services/author.service.spec.ts
@@ -1,12 +1,51 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, getTestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { AuthorService } from './author.service';
+import { singleAuthorMock, authorsMock } from '../utils/mockdata';
+import { environment } from 'src/environments/environment';
 
-describe('AuthorService', () => {
+describe('AuthorService : API Requests', () => {
+  let injector: TestBed;
   let service: AuthorService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AuthorService],
+    });
+
+    injector = getTestBed();
     service = TestBed.inject(AuthorService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('getAuthor() should return an Observable<Author>', () => {
+    service.getAuthor(singleAuthorMock.id).subscribe((authors) => {
+      expect(authors).toEqual(singleAuthorMock);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiUri}/Authors/${singleAuthorMock.id}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(singleAuthorMock);
+  });
+
+  it('getAllAuthors()should return an Observable<Author[]>', () => {
+    service.getAllAuthors().subscribe((authors) => {
+      expect(authors).toEqual(authorsMock);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiUri}/Authors`);
+    expect(req.request.method).toBe('GET');
+    req.flush(authorsMock);
   });
 });
---
+++ src/app/utils/mockdata.ts
@@ -0,0 +1,39 @@
+export const singleAuthorMock = {
+  href: 'http://localhost:4201/Authors/45101208-7e41-4867-bf9a-7d9ce054d2f0',
+  id: '45101208-7e41-4867-bf9a-7d9ce054d2f0',
+  first_name: 'James ',
+  middle_names: 'David',
+  last_name: 'Patterson',
+  name: 'James  David Patterson',
+  about:
+    "Patterson is frequently thought to be the best-selling author in the world today, and has been since 2001. He's also credited as the first author to sell 1 million e-books, and is generally listed as the author with the most New York Times bestsellers.,",
+  version: 'AAAAAAAAF3E=',
+  books: [],
+};
+
+export const authorsMock = [
+  {
+    href: 'http://localhost:4201/Authors/45101208-7e41-4867-bf9a-7d9ce054d2f0',
+    id: '45101208-7e41-4867-bf9a-7d9ce054d2f0',
+    first_name: 'James ',
+    middle_names: 'David',
+    last_name: 'Patterson',
+    name: 'James  David Patterson',
+    about:
+      "Patterson is frequently thought to be the best-selling author in the world today, and has been since 2001. He's also credited as the first author to sell 1 million e-books, and is generally listed as the author with the most New York Times bestsellers.,",
+    version: 'AAAAAAAAF3E=',
+    books: [],
+  },
+  {
+    href: 'http://localhost:4201/Authors/233fa6c5-435d-4457-bf27-f602ba3426d9',
+    id: '233fa6c5-435d-4457-bf27-f602ba3426d9',
+    first_name: 'John',
+    middle_names: 'Middleton',
+    last_name: 'Smith',
+    name: 'John Middleton Smith',
+    about:
+      'Programming with John is a programming volume book where he teaches C# , and Angular',
+    version: 'AAAAAAAAH0E=',
+    books: [],
+  },
+];
