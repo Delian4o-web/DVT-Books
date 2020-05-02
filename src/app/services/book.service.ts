fe484145c5acf500d245d7bd7b7b752c266110b7
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-05-02 21:58:26 +0200

    Add service to create a book

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment';
+import { Book } from '../models/book';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  addBook(book: Book): Observable<Book> {
+    return this.http
+      .post<Book>(`${environment.apiUri}/Books`, book)
+      .pipe(tap((data) => console.log('Book added :' + data)));
+  }
 }
