64a81864c8ceec081ed178f299aad8c11f870c4b
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-05-04 17:24:54 +0200

    View book functionality

--- src/app/users-page/book/view-books/view-books.component.ts
+++ src/app/users-page/book/view-books/view-books.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/app/services/book.service';
+import { Book } from 'src/app/models/book';
 
 @Component({
   selector: 'app-view-books',
   templateUrl: './view-books.component.html',
-  styleUrls: ['./view-books.component.scss']
+  styleUrls: ['./view-books.component.scss'],
 })
 export class ViewBooksComponent implements OnInit {
+  booksList: Book[];
+  book: Book;
+  imageFile: any;
 
-  constructor() { }
+  constructor(public bookService: BookService) {}
 
   ngOnInit(): void {
-  }
+    this.bookService.getAllBooks().subscribe((x) => {
+      this.booksList = x;
+    });
 
+    // this.bookService.getBookPicture('9782001758807').subscribe((x) => {
+    //   this.imageFile = x;
+    //   console.log(this.imageFile);
+    // });
+  }
 }
