866f9720403a38e49ca34eb2b7797e24ab8751fc
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-05-05 15:57:54 +0200

    Add routes for author and book component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,14 @@ import { UsersPageComponent } from './users-page/users-page.component';
 import { TestComponent } from './test/test.component';
 import { BookProfileComponent } from './book-profile/book-profile.component';
 import { AuthorProfileComponent } from './author-profile/author-profile.component';
+import { AuthorComponent } from './users-page/author/author.component';
+import { BookComponent } from './users-page/book/book.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'test', component: TestComponent },
+  { path: 'author', component: AuthorComponent },
+  { path: 'book', component: BookComponent },
   { path: 'book/:id', component: BookProfileComponent },
   { path: 'author/:id', component: AuthorProfileComponent },
   { path: 'profile', component: UsersPageComponent },
