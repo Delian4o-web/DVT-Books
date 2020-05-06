3a2eac3346631075b1b9838daab2d5c17f358e02
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-05-06 22:16:00 +0200

    Add pagination

--- src/app/users-page/search/search.component.ts
+++ src/app/users-page/search/search.component.ts
@@ -11,17 +11,47 @@ export class SearchComponent implements OnInit {
   books: Book[] = [];
   searchString: string;
   errors = '';
+  loadMore = false;
+  newBooks: Book[];
+
+  top = 3;
+  skip = 0;
 
   constructor(private bookService: BookService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.searchString = null;
+    this.books = [];
+  }
+
+  // searchBooks() {
+  //   this.bookService.getBookList(this.searchString).subscribe({
+  //     next: (books) => {
+  //       this.books = books;
+  //     },
+  //     error: (err) => (this.errors = err),
+  //   });
+  // }
 
   searchBooks() {
-    this.bookService.getBookList(this.searchString).subscribe({
-      next: (books) => {
-        this.books = books;
-      },
-      error: (err) => (this.errors = err),
-    });
+    this.bookService
+      .getBookListPaginated(this.searchString, this.top, this.skip)
+      .subscribe((x) => {
+        x.forEach((element) => {
+          this.books.push(element);
+        });
+
+        if (x.length < 3) {
+          this.loadMore = false;
+        }
+      });
+    this.loadMore = true;
+  }
+
+  viewMoreBooks() {
+    if (this.loadMore) {
+      this.skip += 3;
+      this.searchBooks();
+    }
   }
 }
