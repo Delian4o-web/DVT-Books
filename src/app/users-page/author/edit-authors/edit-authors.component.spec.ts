eb6bb7e11b5e40e030317224e86ce644a210e6d7
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-05-07 17:01:51 +0200

    Add unit tests

--- src/app/users-page/author/edit-authors/edit-authors.component.spec.ts
+++ src/app/users-page/author/edit-authors/edit-authors.component.spec.ts
@@ -2,15 +2,38 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { EditAuthorsComponent } from './edit-authors.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Author } from 'src/app/models/author';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AuthorService } from '../../../services/author.service';
+
+class MockService {
+  updateAnAuthor(): Observable<Author> {
+    return {} as Observable<Author>;
+  }
+}
 
 describe('EditAuthorsComponent', () => {
   let component: EditAuthorsComponent;
   let fixture: ComponentFixture<EditAuthorsComponent>;
+  let spy: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [EditAuthorsComponent],
-      imports: [ReactiveFormsModule, FormsModule, HttpClientModule],
+      imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientTestingModule,
+      ],
+      providers: [
+        {
+          AuthorService,
+          useValue: MockService,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EditAuthorsComponent);
@@ -23,5 +46,12 @@ describe('EditAuthorsComponent', () => {
     component = fixture.componentInstance;
     component.ngOnInit();
     fixture.detectChanges();
+  });
+
+  it('should call ngOnInit', () => {
+    spy = spyOn(component, 'ngOnInit').and.callThrough();
+    component.ngOnInit();
+    component.updateAuthorForm.controls.first_name.setValue('Daniel');
+    expect(spy).toHaveBeenCalled();
   });
 });
